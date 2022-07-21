2d06e85ceae6edf070b5cf83e75ccbb47cbff1ee
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-07-21 03:59:25 -0500

    all finsihed vs code, ready to commit

--- game/casting/cast.py
+++ game/casting/cast.py
@@ -1,5 +1,6 @@
 class Cast:
-    """A cast of actors.
+    """
+    A cast of actors.
     The responsibility of a cast is to keep track of actors. It has methods for 
     adding, removing and getting them by a group name.
     Attributes:
@@ -7,11 +8,13 @@ class Cast:
     """
 
     def __init__(self):
-        """Constructs a new game object."""
+        """
+        Constructs a new game object."""
         self._game_objects = {}
         
     def add_game_object(self, group, game_object):
-        """Adds an game object to the given group.
+        """
+        Adds an game object to the given group.
         
         Args:
             group (string): The name of the group.
@@ -24,7 +27,8 @@ class Cast:
             self._game_objects[group].append(game_object)
 
     def get_game_objects(self, group):
-        """Gets the game objects in the given group.
+        """
+        Gets the game objects in the given group.
         
         Args:
             group (string): The name of the group.
@@ -37,7 +41,8 @@ class Cast:
         return results
     
     def get_all_game_objects(self):
-        """Gets all of the game objects in the cast.
+        """
+        Gets all of the game objects in the cast.
         
         Returns:
             List: All of the game objects in the cast.
@@ -48,7 +53,8 @@ class Cast:
         return results
 
     def get_first_game_object(self, group):
-        """Gets the first game object in the given group.
+        """
+        Gets the first game object in the given group.
         
         Args:
             group (string): The name of the group.
@@ -62,7 +68,8 @@ class Cast:
         return result
 
     def remove_game_object(self, group, game_object):
-        """Removes an game object from the given group.
+        """
+        Removes an game object from the given group.
         
         Args:
             group (string): The name of the group.
